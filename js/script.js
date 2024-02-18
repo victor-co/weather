cf9870654b66e120b3508b2b1b03b02140216e46
Victor <victorcorreaa95@gmail.com>
2024-02-18 19:49:51 -0300

    feat: Add dynamic information and css style

--- js/script.js
+++ js/script.js
@@ -10,21 +10,38 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
+const humidityElement = document.querySelector("#umidity span");
 const windElement = document.querySelector("#wind span");
+const weatherContainer = document.querySelector("#weather-data");
 
 // Functions
 const getWeatherData = async (city) => {
   const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
-  const res = await fetch(apiWeatherURL)
+  const res = await fetch(apiWeatherURL);
   const data = await res.json();
 
-  console.log(data);
+  return data;
 };
 
-const showWeatherData = (city) => {
-  getWeatherData(city);
+const showWeatherData = async (city) => {
+  const data = await getWeatherData(city);
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute(
+    "src",
+    `${apiFlags}${data.sys.country}/flat/64.png`
+  );
+  humidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}Km/h`;
+
+  weatherContainer.classList.remove("hide");
 };
 
 // Events
@@ -33,3 +50,4 @@ searchBtn.addEventListener("click", (e) => {
   const city = cityInput.value;
   showWeatherData(city);
 });
+
