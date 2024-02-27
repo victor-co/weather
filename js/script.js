80376168b0b75cc7027dc7a8ed90d0cec0e790bd
Victor <victorcorreaa95@gmail.com>
2024-02-27 17:37:39 -0300

    Add feat: error handling.

--- js/script.js
+++ js/script.js
@@ -14,6 +14,9 @@ const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 const weatherContainer = document.querySelector("#weather-data");
 
+
+const errorDiv = document.querySelector("#error-message");
+
 // Functions
 const getWeatherData = async (city) => {
   const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
@@ -21,10 +24,17 @@ const getWeatherData = async (city) => {
   const res = await fetch(apiWeatherURL);
   const data = await res.json();
 
+  if (data.cod === "404") {
+    throw new Error("Local não encontrado.");
+  } else if (data.cod !== 200) {
+    throw new Error("Informe um local.");
+  }
+
   return data;
 };
 
 const showWeatherData = async (city) => {
+  try {
   const data = await getWeatherData(city);
 
   cityElement.innerText = data.name;
@@ -42,13 +52,36 @@ const showWeatherData = async (city) => {
   windElement.innerText = `${data.wind.speed}Km/h`;
 
   weatherContainer.classList.remove("hide");
+    errorDiv.classList.add("hide");
+  } catch (error) {
+    weatherContainer.classList.add("hide");
+    showError(error.message);
+  }
+};
+
+const showError = (message) => {
+  const errorDiv = document.querySelector("#error-message");
+  errorDiv.innerText = message;
+  errorDiv.classList.remove("hide");
+};
+
+const hideError = () => {
+  const errorDiv = document.querySelector("#error-message");
+  errorDiv.classList.add("hide"); 
 };
 
 // Events
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const city = cityInput.value;
+  const city = cityInput.value.trim();
   showWeatherData(city);
+
+  if (city) {
+    hideError();
+    showWeatherData(city);
+  } else {
+    showError("Informe um local.");
+  }
 });
 
 cityInput.addEventListener("keydown", (e) => {
